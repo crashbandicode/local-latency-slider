d6f183c42fb6b8b398f591b3ba3f3e216b559f9d
crashbandicode <84094612+crashbandicode@users.noreply.github.com>
2026-05-11 08:32:13 -0400

    Fix CpuRegister::x field -> method calls for newer skyline-rs

--- src/ldn/latency_slider/mod.rs
+++ src/ldn/latency_slider/mod.rs
@@ -66,13 +66,13 @@ static CURRENT_INPUT_DELAY: Delay = Delay::default();
 #[skyline::hook(offset = 0x16ccc58, inline)]
 unsafe fn set_online_latency(ctx: &InlineCtx) {
     if ldn::is_local_online() {
-        let auto = *(*ctx.registers[19].x.as_ref() as *mut u8);
+        let auto = *(*ctx.registers[19].x().as_ref() as *mut u8);
         let buffer = CURRENT_INPUT_DELAY.buffer.load(Ordering::SeqCst);
         CURRENT_INPUT_DELAY
             .last_auto
             .store(auto as i8, Ordering::SeqCst);
         if buffer >= 0 {
-            *(*ctx.registers[19].x.as_ref() as *mut u8) = buffer as u8;
+            *(*ctx.registers[19].x().as_ref() as *mut u8) = buffer as u8;
         }
     }
 }
